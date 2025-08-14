82b141a1be97ad02c24b9da022219a93e934843b
Denis Novicov <dema28ster@gmail.com>
2025-08-14 20:45:59 +0200

    refactor

--- backend/src/main/java/com/bumbac/core/config/SecurityConfig.java
+++ backend/src/main/java/com/bumbac/core/config/SecurityConfig.java
@@ -16,6 +16,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+
 
 
 @Configuration
@@ -32,6 +35,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults()) // ← ВКЛЮЧАЕМ CORS
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint((request, response, authException) -> {
                             response.setStatus(403);
