72373f44680e38baba2a4a8e5873a126fc677332
Denis Novicov <dema28ster@gmail.com>
2025-08-18 09:41:39 +0200

    refactor RateLimitFilter

--- backend/src/main/java/com/bumbac/core/config/RateLimiterConfig.java
+++ backend/src/main/java/com/bumbac/core/config/RateLimiterConfig.java
@@ -14,14 +14,14 @@ public class RateLimiterConfig {
 
     private final RateLimitFilter rateLimitFilter;
 
-    @Bean
-    public FilterRegistrationBean<Filter> rateLimiterRegistration() {
-        FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(rateLimitFilter);
-        registration.addUrlPatterns("/api/auth/register"); // Только на регистрацию
-        registration.setOrder(0); // До Spring Security (важно!)
-        return registration;
-    }
+//    @Bean
+//    public FilterRegistrationBean<Filter> rateLimiterRegistration() {
+//        FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+//        registration.setFilter(rateLimitFilter);
+//        registration.addUrlPatterns("/api/auth/register"); // Только на регистрацию
+//        registration.setOrder(0); // До Spring Security (важно!)
+//        return registration;
+//    }
 
     @Bean
     public FilterRegistrationBean<Filter> loginRateLimiterRegistration(LoginRateLimitFilter loginFilter) {
